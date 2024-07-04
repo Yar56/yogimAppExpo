572dc283f5755aab991a0f037a09c8d2b0ab2e26
Yaroslav Vavilin <yarikvavilin@gmail.com>
2024-07-04 19:37:44 +0500

    fix eslint, add no-restricted-imports(2)

--- src/stacks/profileStack/profileSettings/ProfileSettings.tsx
+++ src/stacks/profileStack/profileSettings/ProfileSettings.tsx
@@ -37,7 +37,7 @@ const ProfileSettings = () => {
             userName: profile?.username ?? '',
         },
 
-        onSubmit: async (values, {}) => {
+        onSubmit: async (values) => {
             const { userName } = values;
             setIsLoading(true);
             try {
